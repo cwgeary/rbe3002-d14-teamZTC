a944e4f3d2aa099496cd39c95a823a2f25f49344
zach <zach@ubuntu.(none)>
2014-04-06 22:27:32 -0400

    This is the second try...

---
+++ rbe3002-ws/build/zachlovett_lab2/catkin_generated/package.cmake
@@ -0,0 +1,7 @@
+set(_CATKIN_CURRENT_PACKAGE "zachlovett_lab2")
+set(zachlovett_lab2_MAINTAINER "zach <zach@todo.todo>")
+set(zachlovett_lab2_DEPRECATED "")
+set(zachlovett_lab2_VERSION "0.0.0")
+set(zachlovett_lab2_BUILD_DEPENDS "rospy" "std_msgs")
+set(zachlovett_lab2_RUN_DEPENDS "rospy" "std_msgs")
+set(zachlovett_lab2_BUILDTOOL_DEPENDS "catkin")
---
+++ rbe3002-ws/src/lab3/src/lab3Helpers.py
@@ -0,0 +1,13 @@
+
+from nav_msgs.msg import OccupancyGrid #format for reading the map. Data is stored by row.
+from nav_msgs.msg import GridCells #format for publishing to rviz display
+from geometry_msgs.msg import PoseWithCovarianceStamped #format for reading the start and goal points
+from geometry_msgs.msg import PoseStamped
+
+
+
+
+def makeMap(width, hight, resolution, ocGrid):
+
+
+	return 
